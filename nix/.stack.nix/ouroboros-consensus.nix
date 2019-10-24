252fd2e29d1224e4bd67901b715a806e15029ef0
kderme <k.dermenz@gmail.com>
2019-10-24 17:15:35 +0300

    Use read-write locks in the syscall IO layer

--- nix/.stack.nix/ouroboros-consensus.nix
+++ nix/.stack.nix/ouroboros-consensus.nix
@@ -34,6 +34,7 @@
           (hsPkgs.cardano-ledger)
           (hsPkgs.cardano-prelude)
           (hsPkgs.cborg)
+          (hsPkgs.concurrent-extra)
           (hsPkgs.containers)
           (hsPkgs.cryptonite)
           (hsPkgs.deepseq)
